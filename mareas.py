36f096d52a1324dd41647d5de57970218e9176f7
Juan Fran Martin <juanframaro@gmail.com>
2022-06-16 22:22:24 +0100

    fix: add mareas, refactor mareas, modified .gitignore

--- mareas.py
+++ mareas.py
@@ -2,19 +2,28 @@ from selenium import webdriver
 from bs4 import BeautifulSoup
 from selenium.webdriver.chrome.options import Options
 from time import sleep
+import os, json, calendar, datetime, pandas as pd
 
-link = 'https://tablademareas.com/es/islas-canarias/arrecife-lanzarote'
+from datetime import timedelta
+
+link = "https://www.temperaturadelmar.es/europa/lanzarote/arrecife/tides.html"
+
+# coger forecast y obtener la tabla de mareas
+# luego hacerle un append al forecast
+
+# hacer un main.py que hace estas tareas.
 
 
 class Scraper(object):
+
     def __init__(self):
-        #self.driver = webdriver.PhantomJS('./phantomjs')
+        # self.driver = webdriver.PhantomJS('./phantomjs')
         options = Options()
 
-        options.add_argument('--headless')
-        options.add_argument('window-size=1120x550')
+        options.add_argument("--headless")
+        options.add_argument("window-size=1120x550")
         self.driver = webdriver.Chrome(
-            executable_path=r'/usr/bin/chromedriver', chrome_options=options)
+            executable_path=r"/usr/bin/chromedriver", options=options)
 
     def page_is_loaded(self):
         x = self.driver.execute_script("return document.readyState")
@@ -24,31 +33,77 @@ class Scraper(object):
             return False
 
     def scrape(self):
-        print('Loading...')
+        print("Loading...")
         self.driver.get(link)
+        horasDict = {}
 
-        forecast = {}
         sleep(10)
         while self.page_is_loaded():
             s = BeautifulSoup(self.driver.page_source, "html.parser")
-
-            table = s.find("table", class_="tabulka")
-            tablebody = table.find("tbody")
-            rows = tablebody.find_all("tr")
-
-            for row in rows:
-                cells = row.find_all("td")
-                id = row['id']
-                i = 0
-                if id in ['tabid_0_0_SMER', 'tabid_0_0_dates', 'tabid_0_0_HTSGW']:
-                    forecast[id] = []
+            tables = s.find_all("table", class_="table table-bordered")
+            horas = []
+            for table in tables:
+                tablebody = table.find("tbody")
+                rows = tablebody.find_all("tr")
+                hora = []
+                for row in rows:
+                    cells = row.find_all("td")
                     for cell in cells:
-                        if ('SMER' in id):  # or ('DIRPW' in id):
-                            value = cell.find('span')['title']
-                        else:
-                            value = cell.get_text()
-                        forecast[id].append(value)
-                        i = i + 1
-
+                        if cell.text == "pleamar" or cell.text == "bajamar":
+                            hora.append(cell.text.replace("amar", ""))
+                        elif ":" in cell.text:
+                            last_elem = hora.pop()
+                            text_to_insert = f"{last_elem} {cell.text}h"
+                            hora.insert(len(hora)-1, text_to_insert)
+                horas.append(hora)
+                for hora in horas:
+                     if len(hora) == 3:
+                         hora.append("None")
+            for hora in horas:
+                horasDict[self.get_day_name(horas.index(hora))] = hora
             self.driver.quit()
-            return forecast
+            return horasDict
+
+        # meterlo en otro metodo, convertir a json
+
+    def mareas_to_json(self, mareas):
+        text_file = open("mareas.json", "w")
+        text_to_write = json.dumps(mareas)
+        text_file.write(text_to_write)
+        text_file.close()
+
+    # convertir a dataframe, otro metodo
+    def jsonfc_to_df(self):
+        df = pd.read_json("mareas.json", orient="records")
+        return df
+
+    def df_to_txt(self, df):
+        if os.path.exists("mareas.txt"):
+            os.remove("mareas.txt")
+        with open("mareas.txt", "a") as f:
+            dfAsString = df.to_string(header=True, index=False)
+            f.write(dfAsString)
+
+    def get_day_name(self, add):
+        day = datetime.date.today() + datetime.timedelta(days=add)
+        day_matches = {
+            "Monday": "Mo",
+            "Tuesday": "Tu",
+            "Wednesday": "We",
+            "Thursday": "Th",
+            "Friday": "Fr",
+            "Saturday": "Sa",
+            "Sunday": "Sa",
+        }
+        day_name = day_matches[day.strftime("%A")]
+        day_number = day.strftime("%d")
+
+        return day_name + day_number
+
+
+if __name__ == "__main__":
+    scraper = Scraper()
+    mareas = scraper.scrape()
+    scraper.mareas_to_json(mareas)
+    df = scraper.jsonfc_to_df()
+    scraper.df_to_txt(df)
